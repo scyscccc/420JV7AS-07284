7fdb43660591e5d33839f795c5ee82f836bae72e
Felix <felix@felixsoum.com>
2020-06-02 03:20:58 -0400

    Add SQLite Project

---
+++ SQLite Project/Assets/Scripts/Weapon.cs
@@ -0,0 +1,16 @@
+﻿public class Weapon
+{
+    public string Name { get; set; }
+    public int Cost { get; set; }
+
+    public Weapon(string name, int cost)
+    {
+        Name = name;
+        Cost = cost;
+    }
+
+    public override string ToString()
+    {
+        return $"{Name}: {Cost}";
+    }
+}
